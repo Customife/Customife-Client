959529062e4517829a7900f80b70b8fa91cbc6ed
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-04-02 15:30:28 +0900

    Revert "chore: Storybook 설정"
    This reverts commit 3ae2d0c0731c39e3664120e2471da21fb065c0b9.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
         es6: true,
         node: true,
     },
-    extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:prettier/recommended',
+    ],
     globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
