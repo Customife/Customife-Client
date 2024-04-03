9b669979c9d346b4131d2f7bacd1d8f39c74042d
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-04-03 20:13:22 +0900

    feat: IntroPage 화면 구현
    IntroPage의 기본적인 화면 구현을 진행하였다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Global } from '@emotion/react';
 import React from 'react';
 
+import { IntroPage } from './pages';
 import { GlobalStyle } from './styles';
 
 const App = () => {
     return (
         <div className="container">
             <Global styles={GlobalStyle} />
+            <IntroPage />
         </div>
     );
 };
---
+++ src/pages/IntroPage/IntroPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Logo } from '../../components';
+
+export const IntroPage = () => {
+    return <Logo name="Customife"></Logo>;
+};
