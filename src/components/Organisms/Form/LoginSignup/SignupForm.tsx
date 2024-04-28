852a92760dcde65fdbfa40a046177a8b09946af1
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-04-28 18:40:04 +0900

    feat: 비밀번호 유효성 검사 기능 구현
    비밀번호가 8자리 이상인지, 그리고 대소문자, 숫자, 특수문자를 포함하는지 검사하는 기능을 구현하였다.

--- src/components/Organisms/Form/LoginSignup/SignupForm.tsx
+++ src/components/Organisms/Form/LoginSignup/SignupForm.tsx
@@ -7,7 +7,9 @@ const Colors = {
     green: 'rgb(118, 171, 174)',
 };
 
-const ErrMessages = {
+const Messages = {
+    passwordLength: '비밀번호는 8자리 이상이어야 합니다.',
+    passwordCharacter: '비밀번호는 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.',
     passwordCheckErr: '비밀번호가 다릅니다.',
 };
 
@@ -15,14 +17,31 @@ export const SignupForm = () => {
     const [password, setPassword] = useState('');
     const [passwordCheck, setPasswordCheck] = useState('');
 
+    const [isPasswordLength, setIsPasswordLength] = useState(false);
+    const [isPasswordCharacter, setIsPasswordCharacter] = useState(false);
     const [isPasswordCheck, setIsPasswordCheck] = useState(false);
 
-    const [passwordErrMessage, setPasswordErrMessage] = useState('');
+    const handlePassword = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            const { value } = event.target;
+            const lengthRegex = /^.{8,}$/;
+            const characterRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/;
+            setPassword(value);
 
-    const handlePassword = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.target;
-        setPassword(value);
-    }, []);
+            if (!lengthRegex.test(value)) {
+                setIsPasswordLength(false);
+            } else {
+                setIsPasswordLength(true);
+            }
+
+            if (!characterRegex.test(value)) {
+                setIsPasswordCharacter(false);
+            } else {
+                setIsPasswordCharacter(true);
+            }
+        },
+        [password],
+    );
 
     const validatePasswordCheck = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = event.target;
@@ -48,6 +67,8 @@ export const SignupForm = () => {
                 placeholder="비밀번호"
                 color={Colors.gray}
             />
+            <p css={isPasswordLength ? SuccessStyle : FailedStyle}>{Messages.passwordLength}</p>
+            <p css={isPasswordCharacter ? SuccessStyle : FailedStyle}>{Messages.passwordCharacter}</p>
             <BasicInput
                 type="password"
                 name="password_check"
@@ -55,8 +76,16 @@ export const SignupForm = () => {
                 placeholder="비밀번호 확인"
                 color={Colors.gray}
             />
-            <p>{!isPasswordCheck && ErrMessages.passwordCheckErr}</p>
+            <p css={isPasswordCheck ? SuccessStyle : FailedStyle}>{Messages.passwordCheckErr}</p>
             <BasicInput type="submit" value="회원가입" color={Colors.green} />
         </form>
     );
 };
+
+const SuccessStyle = css`
+    color: #13661b;
+`;
+
+const FailedStyle = css`
+    color: #db3d27;
+`;
