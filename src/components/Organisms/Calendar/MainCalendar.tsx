027a72b948972182f62af47ae507adbd9f9fe41d
sieunp06 <psieun0606@gmail.com>
2024-07-14 20:29:46 +0900

    delete: react-calendar 관련 파일 삭제

--- src/components/Organisms/Calendar/MainCalendar.tsx
+++ src/components/Organisms/Calendar/MainCalendar.tsx
@@ -1,8 +1,6 @@
 import { css } from '@emotion/react';
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import '../../../styles/components/MainCalendar.css';
 
 export const MainCalendar = () => {
     const [value, setValue] = useState(new Date());
@@ -11,9 +9,5 @@ export const MainCalendar = () => {
         setValue(value);
     };
 
-    return (
-        <div>
-            <Calendar locale="en" onChange={onChange} value={value}></Calendar>
-        </div>
-    );
+    return <div></div>;
 };
